0c53b08822bee19c1c5423e7dea5f440f21ab933
IziDeLuna <izideluna@gmail.com>
2017-04-15 10:40:44 -0400

    Fixed a bug that wasn't allowing it to work

--- addeditdialog.cpp
+++ addeditdialog.cpp
@@ -2,19 +2,24 @@
 #include "ui_addeditdialog.h"
 #include <QDialog>
 
+//Basic constructor
 AddEditDialog::AddEditDialog(QWidget *parent) : QDialog(parent), ui(new Ui::AddEditDialog) {
     ui->setupUi(this);
 }
 
+//Destructor for the dialog
 AddEditDialog::~AddEditDialog() {
     delete ui;
 }
 
+//Closes the dialog box
 void AddEditDialog::on_cancelButton_clicked() {
     close();
 }
 
 
 void AddEditDialog::on_addCustButton_clicked() {
-
+    //Data inside of here needs to get attached straight
+    //to the data base so when added, the customer will show up
+    //for future appointments
 }
