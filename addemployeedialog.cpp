32d03d32ef189b53e6a8ad990583b2f99d1ad8ff
James Albu <James Albu>
2017-04-16 23:09:58 -0400

    Added trainerdialog addtrainer db function

--- addemployeedialog.cpp
+++ addemployeedialog.cpp
@@ -2,15 +2,7 @@
 #include "ui_addemployeedialog.h"
 #include <QSqlQuery>
 #include <QDebug>
-#include <string>
 #include <QMessageBox>
-
-/*
-#include <QStandardPaths>
-#include <QtSql>
-#include <QSqlDatabase>
-#include <QFileInfo>
-*/
 
 addEmployeeDialog::addEmployeeDialog(QWidget *parent) :
     QDialog(parent),
@@ -18,11 +10,6 @@ addEmployeeDialog::addEmployeeDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     this->setWindowTitle("Add a new Employee");
-    /*
-    //Open database
-    *mydb = QSqlDatabase::addDatabase("QSQLITE");
-    mydb->setDatabaseName("/Users/James/Github/MasterGymTrainer")
-    */
 }
 
 addEmployeeDialog::~addEmployeeDialog()
@@ -39,12 +26,14 @@ void addEmployeeDialog::on_addEmployeeButton_clicked()
 {
     int eid = 0;
     int level = 2;
+
     QString firstname = ui->firstNameBox->text();
     QString lastname = ui->lastNameBox->text();
     QString username = ui->usernameBox->text();
     QString password = ui->passwordBox->text();
 
     QSqlQuery emQry;
+
     emQry.exec("SELECT MAX(eid) FROM userlogin");
     //Further testing my be needed...
     while (emQry.next()){
@@ -60,19 +49,21 @@ void addEmployeeDialog::on_addEmployeeButton_clicked()
     emQry.bindValue(":firstname",firstname);
     emQry.bindValue(":lastname", lastname);
 
-    //emQry fails if duplicate, so this is duplicate detecting code for now
+    //FIXME: emQry fails if duplicate, so this is duplicate detecting code for now
     if(!emQry.exec()){
         qDebug() << "Did not insert into table employee";
         QMessageBox::warning(this, "Warning", "Possible duplicate in employee database");
     }
 
-    //Add to userlogin table, does not account for duplicates yet
+    //Insert into userlogin table
     emQry.prepare("INSERT INTO userlogin (eid,username,password,level)"
                   "VALUES (:eid,:username,:password,:level)");
     emQry.bindValue(":eid",eid);
     emQry.bindValue(":username",username);
     emQry.bindValue(":password",password);
     emQry.bindValue(":level",level);
+
+    //FIXME: emQry fails if duplicate, so this is duplicate detecting code for now
     if(!emQry.exec()){
         qDebug() << "Did not insert into table userlogin";
         QMessageBox::warning(this, "Warning", "Possible duplicate in username database");
--- trainerdialog.cpp
+++ trainerdialog.cpp
@@ -1,5 +1,8 @@
 #include "trainerdialog.h"
 #include "ui_trainerdialog.h"
+#include <QSqlQuery>
+#include <Qdebug>
+#include <QMessageBox>
 
 TrainerDialog::TrainerDialog(QWidget *parent) :
     QDialog(parent),
@@ -21,5 +24,49 @@ void TrainerDialog::on_cancelButton_clicked()
 
 void TrainerDialog::on_addTrainerButton_clicked()
 {
-    //Add code to add Trainer to database
+    int eid = 0;
+    int level = 1;
+
+    QString firstname = ui->firstNameBox->text();
+    QString lastname = ui->lastNameBox->text();
+    QString username = ui->usernameBox->text();
+    QString password = ui->passwordBox->text();
+
+    QSqlQuery trQry;
+
+    trQry.exec("SELECT MAX(eid) FROM userlogin");
+    while (trQry.next()){
+        eid = trQry.value(0).toInt();
+    }
+
+    //Increment eid to store new employee
+    eid = eid + 1;
+
+    //Add to trainer table
+    trQry.prepare("INSERT INTO trainer (eid,firstname,lastname)"
+                  "VALUES (:eid,:firstname,:lastname)");
+    trQry.bindValue(":eid",eid);
+    trQry.bindValue(":firstname",firstname);
+    trQry.bindValue(":lastname",lastname);
+
+    //FIXME: trQry fails if duplicate, so this is duplicate detecting code for now
+    if(!trQry.exec()){
+        qDebug() << "Did not insert into trainer";
+        QMessageBox::warning(this,"Warning", "Possible duplicate in trainer database");
+    }
+
+    //Add to userlogin table
+    trQry.prepare("INSERT INTO userlogin (eid,username,password,level)"
+                  "VALUES (:eid,:username,:password,:level)");
+    trQry.bindValue(":eid",eid);
+    trQry.bindValue(":username",username);
+    trQry.bindValue(":password",password);
+    trQry.bindValue(":level",level);
+
+    //FIXME: trQry fails if duplicate, so this is duplicate detecting code for now
+    if(!trQry.exec()){
+        qDebug() << "Did not insert into userlogin";
+        QMessageBox::warning(this,"Warning", "Possible duplicate in userlogin database");
+    }
+    close();
 }
