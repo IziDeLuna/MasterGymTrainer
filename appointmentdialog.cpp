9f150bf972c6555ffb366582acb6936c1c056aa2
James Albu <James Albu>
2017-04-19 22:13:07 -0400

    Finished linking appt table to apointment dialog

--- appointmentdialog.cpp
+++ appointmentdialog.cpp
@@ -21,24 +21,29 @@ void appointmentDialog::on_cancelButton_clicked(){
 }
 
 void appointmentDialog::on_addApptButton_clicked(){
-    QString cid = ui->custID->text();
-    QString eid = ui->employID->text();
+    QString cid = ui->custIDBox->text();
+    QString eid = ui->employIDBox->text();
     QString date = ui->apptDateBox->text();
     QString time = ui->apptTimeBox->text();
-
+    QString datetime = date + " " + time;
     QSqlQuery qry;
 
     //Add to table
-    qry.prepare("INSERT INTO appt (eid, cid,appdate)"
-                "VALUES (:eid,:cid,:appdate");
+    qry.prepare("INSERT INTO appt (eid,cid,appdate)"
+                "VALUES (:eid,:cid,:appdate)");
     qry.bindValue(":eid",eid);
     qry.bindValue(":cid",cid);
     //need to find out how to query datetime
-    qry.bindValue(":appdate",date + "/2017 " + time);
+    qry.bindValue(":appdate",datetime);
+
+    //qry.bindValue(":appdate",date + "/2017 " + time);
 
     if(!qry.exec()) {
         qDebug() << "Did not insert into table appt";
         QMessageBox::warning(this,"Warning", "Possible Dupliate in database");
+    }
+    else{
+        QMessageBox::information(this,"Employee","Appointment added successfully!");
     }
     close();
 }
